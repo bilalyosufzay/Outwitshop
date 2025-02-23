d8a4bb13775ced5cbb097f8fb15476d65181a9e0
bilalyosufzay <Yosufzayb@gmail.com>
2025-02-24 01:23:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size limit to 1000 KB
   },
 }));
